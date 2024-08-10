21b388cd4faa08c07074eea2fac5c7728dbd48d3
gabrielalmir <gabr.almir@gmail.com>
2024-08-10 18:03:18 -0300

    feat: change sample prompt

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import path from 'path';
 
 async function main() {
     const prompt = new CreatePromptDTO({
-        prompt: '1girl, irelia, league of legends, (wallpaper), (masterpiece), (safe)',
+        prompt: '1girl, souryuu asuka langley, neon genesis evangelion, plugsuit, pilot suit, red bodysuit, sitting, crossing legs, black eye patch, cat hat, throne, symmetrical, looking down, from bottom, looking at viewer, outdoors, (wallpaper), (masterpiece), (safe)',
         negative: 'longbody, lowres, bad anatomy, bad hands, missing fingers, pubic hair, extra digit, fewer digits, cropped, worst quality, low quality, very displeasing, badanatomy, nsfw, lowres, (bad), text, error, fewer, extra, missing, worst quality, jpeg artifacts, low quality, watermark, unfinished, displeasing, oldest, early, chromatic aberration, signature, extra digits, artistic error, username, scan, [abstract]',
         resolution: {
             width: 1280,
